06c7e2b837ceef7895158fd3710ea8fafc061447
Abbas Rizvi <abbasriz1234567890@gmail.com>
2025-02-16 19:43:05 +0500

    loading screen implemented

--- app/src/main/java/com/example/mcqbasedquizapp/MainActivity.java
+++ app/src/main/java/com/example/mcqbasedquizapp/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.mcqbasedquizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Animation animate_logo;
+    ImageView logo;
+
+
+
+    private void init(){
+        animate_logo=AnimationUtils.loadAnimation(this,R.anim.rotate_ball);
+        logo=findViewById(R.id.logo_img_id);
+        logo.setAnimation(animate_logo);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +38,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        init();
+
+        new Handler()
+                .postDelayed(()->{
+                    startActivity(new Intent(MainActivity.this, Home.class));
+                    finish();
+                }, 3000);
+
+
     }
 }
