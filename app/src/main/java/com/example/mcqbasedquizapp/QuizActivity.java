a55e115c5f3ae664cef975023cf5aa12b3842335
Abbas Rizvi <abbasriz1234567890@gmail.com>
2025-02-17 23:50:58 +0500

    Quiz page completed with intent

--- app/src/main/java/com/example/mcqbasedquizapp/QuizActivity.java
+++ app/src/main/java/com/example/mcqbasedquizapp/QuizActivity.java
@@ -28,9 +28,10 @@ class Quizes{
 
     Quizes(){
         score=new int[]{
-                0,0,0,0,0,0,0,0,0,0
+                0,0,0,0,0,0,0,0,0,0,0
         };
         questions = new String[] {
+                " ",
                 "When was Osama Bin Laden born?",
                 "What is the capital of France?",
                 "What is 2 + 2?",
@@ -41,10 +42,11 @@ class Quizes{
                 "What is the chemical symbol for water?",
                 "Which animal is known as the King of the Jungle?",
                 "What is the largest ocean on Earth?",
-                " "
+
         };
 
         options = new String[][] {
+                {" "," "," "," "},
                 {"1980", "1985", "1990", "1995"},
                 {"Berlin", "Madrid", "Paris", "Rome"},
                 {"1", "2", "3", "4"},
@@ -55,11 +57,12 @@ class Quizes{
                 {"H2", "H2O", "O2", "CO2"},
                 {"Tiger", "Elephant", "Lion", "Cheetah"},
                 {"Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"},
-                {" "," "," "," "}
+
         };
 
 
         answers = new String[] {
+                " ",
                 "1990",
                 "Paris",
                 "4",
@@ -70,7 +73,7 @@ class Quizes{
                 "H2O",
                 "Lion",
                 "Pacific Ocean",
-                " "
+
         };
         totalq=10;
 
@@ -160,42 +163,47 @@ public class QuizActivity extends AppCompatActivity {
             return insets;
         });
 
+//        Intent i=getIntent();
+//        String user_name=i.getStringExtra("userinput").toString();
+
         init();
         Quizes quizes=new Quizes();
 
         int []question_number2 = new int[1];
-        question_number2[0]=0;
+        question_number2[0]=1;
 
 
 
 
         question_displayer(question_number2[0],display_question,quizes,option_1,option_2,option_3,option_4, radio_grp_identifier);
-//        question_checker(quizes, radio_grp_identifier,0);
-        question_number2[0]=1;
+        String garbage = question_number2[0] + "";
+        current_quesion.setText(garbage);
+
         changer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(question_number2[0]==quizes.totalq-1) {
-                    changer.setText(R.string.finish);
-                }
-                if(question_number2[0]==quizes.totalq){
-                    Intent i=new Intent(QuizActivity.this,ScoreActivity.class);
-                    int sent_score=quizes.getsum();
-                    Toast.makeText(QuizActivity.this, sent_score, Toast.LENGTH_SHORT).show();
-                    i.putExtra("user_score",sent_score);
+                question_checker(quizes, radio_grp_identifier, question_number2[0]);
+
+                if (question_number2[0] < quizes.totalq) { // Prevent going out of bounds
+                    question_number2[0]++;
+                    question_displayer(question_number2[0], display_question, quizes, option_1, option_2, option_3, option_4, radio_grp_identifier);
+                    current_quesion.setText(String.valueOf(question_number2[0]));
+
+                    if (question_number2[0] == quizes.totalq) {
+                        changer.setText(R.string.finish); // Update button text on the last question
+                    }
+                } else {
+                    // Logic for finishing the quiz
+                    Toast.makeText(QuizActivity.this, "Quiz Finished!", Toast.LENGTH_SHORT).show();
+                    int finalScore = quizes.getsum();
+                    Intent i=new Intent(QuizActivity.this, ScoreActivity.class);
+                    i.putExtra("score",finalScore);
                     startActivity(i);
                     finish();
                 }
-                question_checker(quizes, radio_grp_identifier,question_number2[0]);
-                ++question_number2[0];
-                question_displayer(question_number2[0],display_question,quizes,option_1,option_2,option_3,option_4, radio_grp_identifier);
-                String garbage=question_number2[0]+"";
-//                Toast.makeText(QuizActivity.this, garbage, Toast.LENGTH_SHORT).show();
-                current_quesion.setText(garbage);
-
             }
-
         });
+
 
         previd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -205,6 +213,7 @@ public class QuizActivity extends AppCompatActivity {
                     String garbage2=question_number2[0]+"";
                     current_quesion.setText(garbage2);
                     question_displayer(question_number2[0],display_question,quizes,option_1,option_2,option_3,option_4, radio_grp_identifier);
+                    changer.setText(R.string.nextt);
                 }
             }
         });
@@ -217,6 +226,7 @@ public class QuizActivity extends AppCompatActivity {
                     String garbage2=question_number2[0]+"";
                     current_quesion.setText(garbage2);
                     question_displayer(question_number2[0],display_question,quizes,option_1,option_2,option_3,option_4, radio_grp_identifier);
+                    changer.setText(R.string.nextt);
                 }
             }
         });
