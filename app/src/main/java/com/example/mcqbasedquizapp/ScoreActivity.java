6833c8238055d79e041cfa295cf2188796edca7b
Abbas Rizvi <abbasriz1234567890@gmail.com>
2025-02-18 02:34:40 +0500

    Skeleton working done. Only Result Page GUI left

--- app/src/main/java/com/example/mcqbasedquizapp/ScoreActivity.java
+++ app/src/main/java/com/example/mcqbasedquizapp/ScoreActivity.java
@@ -2,9 +2,15 @@ package com.example.mcqbasedquizapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,9 +18,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ScoreActivity extends AppCompatActivity {
     TextView userscore;
+    TextView n;
+    Button share;
+    ActivityResultLauncher<Intent> sharer;
 
     private void init(){
         userscore=findViewById(R.id.userscore);
+        n=findViewById(R.id.usernamescore);
+        share=findViewById(R.id.share);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +39,30 @@ public class ScoreActivity extends AppCompatActivity {
         });
         init();
 
+        sharer=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),(result)->{
+                if(result.getResultCode()==RESULT_OK){
+                    Toast.makeText(this, "shared successfully", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(this, "Share action cancelled", Toast.LENGTH_SHORT).show();
+                }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Intent.ACTION_SEND);
+                i.setType("image/*");
+                sharer.launch(i);
+            }
+        });
+
         Intent i=getIntent();
         int score=i.getIntExtra("score",0);
         String name=i.getStringExtra("name");
         userscore.setText(String.valueOf(score));
+        n.setText(name);
+
+
     }
 }
